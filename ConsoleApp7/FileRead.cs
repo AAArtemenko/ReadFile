2cc8ca50f8ba65a356cf01e1ccabe2054fac5472
Artsiom Artsiomenka <AArtsiomenka@gomel.iba.by>
2018-12-13 16:23:02 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp7/FileRead.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class FileRead
+    {
+        private string DirName { get; set; }
+
+        public FileRead(string DirName)
+        {
+            this.DirName = DirName;
+        }
+
+        public void GetFiles()
+        {
+            if (Directory.Exists(DirName))
+            {
+                Console.WriteLine("Files:");
+                string[] files = Directory.GetFiles(DirName);
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            else
+                Console.WriteLine("Directory not found");
+        }
+    }
+}
---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter directiry: ");
+            FileRead txt = new FileRead(Console.ReadLine());           
+            txt.GetFiles();
+            Console.WriteLine("Enter path to the file: ");
+            try
+            {
+                using (StreamReader streamWriter = File.OpenText(Console.ReadLine()))
+                {
+                    Console.WriteLine(streamWriter.ReadLine());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
